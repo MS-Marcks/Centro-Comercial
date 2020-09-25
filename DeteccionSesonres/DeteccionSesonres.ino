b21a83fdc3d0728e1a3958933126cc2577f84234
MS-Marcks <gersonramos46@gmail.com>
2020-09-25 12:18:58 -0600

    se subio el proyecto de arduino, y las base de datos y los modelos

---
+++ DeteccionSesonres/DeteccionSesonres.ino
@@ -0,0 +1,116 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <DHT.h>
+
+#define FIREBASE_HOST "centro-comercial-617c3.firebaseio.com"
+#define FIREBASE_AUTH "V4ypsu2QTycLJZ3TJxempMvIqYkE6ae2K26ebNWK"
+#define WIFI_SSID "MarcksPC"
+#define WIFI_PASSWORD "12345678"
+
+#define DHTPIN 5
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+long OnTimedht = 350;
+long OffTimedht = 350;
+bool Statedht = true;
+unsigned long previousMillisdht;
+
+
+#define SENSOR1 16
+long OnTimesensor1 = 350;
+long OffTimesensor1 = 350;
+bool Statesensor1 = true;
+boolean valsensor1;
+unsigned long previousMillissensor1;
+
+
+#define SENSOR2 4
+long OnTimesensor2 = 350;
+long OffTimesensor2 = 350;
+bool Statesensor2 = true;
+boolean valsensor2;
+unsigned long previousMillissensor2;
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.set("tiendas", "");
+  pinMode (SENSOR1, INPUT);
+  pinMode (SENSOR2, INPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  DetectoHumTemp1(currentMillis);
+  Sensor1(currentMillis);
+  Sensor2(currentMillis);
+
+}
+void DetectoHumTemp1(unsigned long currentMillis) {
+  if ((Statedht == true) && (currentMillis - previousMillisdht >= OnTimedht))
+  {
+    Statedht = false;
+    previousMillisdht = currentMillis;
+    float temp = dht.readTemperature();
+    float hum = dht.readHumidity();
+    if (isnan(temp) || isnan(hum)) {
+      Serial.println("Error obteniendo datos");
+    } else {
+      Firebase.setFloat("tiendas/0/temp", temp);
+      Firebase.setFloat("tiendas/0/hum", hum);
+    }
+  }
+  else if ((Statedht == false) && (currentMillis - previousMillisdht >= OffTimedht))
+  {
+    Statedht = true;
+    previousMillisdht = currentMillis;
+  }
+}
+
+void Sensor1(unsigned long currentMillis) {
+  if ((Statesensor1 == true) && (currentMillis - previousMillissensor1 >= OnTimesensor1))
+  {
+    Statesensor1 = false;
+    previousMillissensor1 = currentMillis;
+    valsensor1 = !digitalRead(SENSOR1);
+    //estadoanterior= val;
+    //if()
+    Firebase.setInt("estaciomanieto/0/ocupado", valsensor1);
+
+  }
+  else if ((Statesensor1 == false) && (currentMillis - previousMillissensor1 >= OffTimesensor1))
+  {
+    Statesensor1 = true;
+    previousMillissensor1 = currentMillis;
+  }
+}
+
+void Sensor2(unsigned long currentMillis) {
+  if ((Statesensor2 == true) && (currentMillis - previousMillissensor2 >= OnTimesensor2))
+  {
+    Statesensor2 = false;
+    previousMillissensor1 = currentMillis;
+    valsensor2 = !digitalRead(SENSOR2);
+    //estadoanterior= val;
+    //if()
+    Firebase.setInt("estaciomanieto/1/ocupado", valsensor2);
+
+  }
+  else if ((Statesensor2 == false) && (currentMillis - previousMillissensor2 >= OffTimesensor2))
+  {
+    Statesensor2 = true;
+    previousMillissensor2 = currentMillis;
+  }
+}
---
+++ Puertas/Puertas.ino
@@ -0,0 +1,146 @@
+//================================================
+// Librerias
+//================================================
+#include <NewPing.h>
+#include <pt.h>
+#include <Servo.h>
+
+//================================================
+// PINES
+//================================================
+#define servopuertaPin 8
+#define servoestacionPin 9
+
+#define trigEstacionPin 13
+#define echoEstacionPin 12
+
+#define trigPuertaPin 4
+#define echoPuertaPin 3
+
+//================================================
+// VARIABLES GLOBALES
+//================================================
+#define MAX_DISTANCE  200
+
+Servo servoMotorEstacion;
+Servo servoMotorPuerta;
+
+NewPing sonarEstacionamiento(trigEstacionPin, echoEstacionPin, MAX_DISTANCE);
+NewPing sonarPuerta(trigPuertaPin, echoPuertaPin, MAX_DISTANCE);
+
+bool HabilitarEstacion = false, habilitoEstacion = false, HabilitarPuerta = false, habilitoPuerta = false;
+int iE = 0, kE = 0, iP = 0, kP = 0;
+
+//================================================
+// HILOS EN EL PROGRAMA
+//================================================
+struct pt ProximetroEstacionamiento;
+struct pt ServoMotorEstacionamiento;
+
+struct pt ProximetroPuertaPrincipal;
+struct pt ServoMotorPuertaPrincipal;
+
+void setup() {
+  Serial.begin(9600);
+
+  PT_INIT(&ServoMotorEstacionamiento);
+  PT_INIT(&ProximetroEstacionamiento);
+
+  PT_INIT(&ServoMotorPuertaPrincipal);
+  PT_INIT(&ProximetroPuertaPrincipal);
+
+  servoMotorEstacion.attach(servoestacionPin);
+  servoMotorPuerta.attach(servopuertaPin);
+
+  pinMode(trigEstacionPin, OUTPUT);
+  pinMode(echoEstacionPin, INPUT);
+
+  pinMode(trigPuertaPin, OUTPUT);
+  pinMode(echoEstacionPin, INPUT);
+
+  servoMotorEstacion.write(90);
+  servoMotorPuerta.write(90);
+}
+
+void loop() {
+  fun_ProximetroEstacionamiento(&ProximetroEstacionamiento);
+  fun_ProximetroPuerta(&ProximetroPuertaPrincipal);
+  fun_ServoEstacionamiento(&ServoMotorEstacionamiento);
+  fun_ServoPuerta(&ServoMotorPuertaPrincipal);
+}
+
+void fun_ProximetroEstacionamiento(struct pt *pt) {
+  PT_BEGIN(pt);
+  static long t = 0;
+  do {
+    t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 50);
+    long distanciaE = sonarEstacionamiento.ping_cm();
+    if ( distanciaE < 15 && distanciaE != 0) {
+      HabilitarEstacion = true;
+    } else {
+      HabilitarEstacion = false;
+    }
+  } while (true);
+  PT_END(pt);
+}
+
+void fun_ProximetroPuerta(struct pt *pt) {
+  PT_BEGIN(pt);
+  static long t = 0;
+  do {
+    t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 50);
+    long distanciaP = sonarPuerta.ping_cm();
+    if ( distanciaP < 15 && distanciaP != 0) {
+      HabilitarPuerta = true;
+    } else {
+      HabilitarPuerta = false;
+    }
+  } while (true);
+  PT_END(pt);
+}
+
+void fun_ServoEstacionamiento(struct pt *pt) {
+  PT_BEGIN(pt);
+  static long t = 0;
+  do {
+    if (HabilitarEstacion == true && habilitoEstacion == false) {
+      for (iE = 90; iE >= 0 ; iE -= 15) {
+        servoMotorEstacion.write(iE);
+        t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 100);
+      }
+      habilitoEstacion = true;
+    } else if (HabilitarEstacion == false && habilitoEstacion == true) {
+      t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 5000);
+      for (kE = 0; kE <= 90 ; kE += 15) {
+        servoMotorEstacion.write(kE);
+        t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 100);
+      }
+      habilitoEstacion = false;
+    }
+    t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 1000);
+  } while (true);
+  PT_END(pt);
+}
+
+void fun_ServoPuerta(struct pt *pt) {
+  PT_BEGIN(pt);
+  static long t = 0;
+  do {
+    if (HabilitarPuerta == true && habilitoPuerta == false) {
+      for (iP = 90; iP >= 0 ; iP -= 15) {
+        servoMotorPuerta.write(iP);
+        t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 100);
+      }
+      habilitoPuerta = true;
+    } else if (HabilitarPuerta == false && habilitoPuerta == true) {
+      t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 5000);
+      for (kP = 0; kP <= 90 ; kP += 15) {
+        servoMotorPuerta.write(kP);
+        t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 100);
+      }
+      habilitoPuerta = false;
+    }
+    t = millis(); PT_WAIT_WHILE(pt, (millis() - t) < 1000);
+  } while (true);
+  PT_END(pt);
+}
